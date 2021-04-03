c66a6fc9a57a90f4e27ab4f3e18bfb838e5aef3e
Tom Golden <8162045+tbjgolden@users.noreply.github.com>
2021-04-02 22:55:29 -0500

    feat: initial working version

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,138 +1,83 @@
-import min from '.'
+import colornorm from '.'
 
-test('should lowercase keywords', () => {
-  expect(min('RED')).toBe('red')
-})
+// prettier-ignore
+const inputs = [
+  ["RED", "hsl(0 100% 50%)", "#f00", "rgb(255 0 0)", "hsl(0 100% 50%)", "hwb(0 0% 0%)", "lab(53% 80 67)", "lch(53% 105 40)", "device-cmyk(0% 100% 100% 0%)"],
+  ["#f00", "hsl(0 100% 50%)", "#f00", "rgb(255 0 0)", "hsl(0 100% 50%)", "hwb(0 0% 0%)", "lab(53% 80 67)", "lch(53% 105 40)", "device-cmyk(0% 100% 100% 0%)"],
+  ["#ff0000", "hsl(0 100% 50%)", "#f00", "rgb(255 0 0)", "hsl(0 100% 50%)", "hwb(0 0% 0%)", "lab(53% 80 67)", "lch(53% 105 40)", "device-cmyk(0% 100% 100% 0%)"],
+  ["rgb(255,0,0)", "hsl(0 100% 50%)", "#f00", "rgb(255 0 0)", "hsl(0 100% 50%)", "hwb(0 0% 0%)", "lab(53% 80 67)", "lch(53% 105 40)", "device-cmyk(0% 100% 100% 0%)"],
+  ["rgba(255, 0, 0, 1)", "hsl(0 100% 50%)", "#f00", "rgb(255 0 0)", "hsl(0 100% 50%)", "hwb(0 0% 0%)", "lab(53% 80 67)", "lch(53% 105 40)", "device-cmyk(0% 100% 100% 0%)"],
+  ["hsl(0, 100%, 50%)", "hsl(0 100% 50%)", "#f00", "rgb(255 0 0)", "hsl(0 100% 50%)", "hwb(0 0% 0%)", "lab(53% 80 67)", "lch(53% 105 40)", "device-cmyk(0% 100% 100% 0%)"],
+  ["hsla(0, 100%, 50%, 1)", "hsl(0 100% 50%)", "#f00", "rgb(255 0 0)", "hsl(0 100% 50%)", "hwb(0 0% 0%)", "lab(53% 80 67)", "lch(53% 105 40)", "device-cmyk(0% 100% 100% 0%)"],
+  ["hsla(0, 100%, 50%, .5)", "hsl(0 100% 50%/0.5)", "#ff000080", "rgb(255 0 0/0.5)", "hsl(0 100% 50%/0.5)", "hwb(0 0% 0%/0.5)", "lab(53% 80 67/0.5)", "lch(53% 105 40/0.5)", "device-cmyk(0% 100% 100% 0%/0.5)"],
+  ["#FFFFFF", "hsl(0 0% 100%)", "#fff", "rgb(255 255 255)", "hsl(0 0% 100%)", "hwb(0 100% 0%)", "lab(100% 0 0)", "lch(100% 0 297)", "device-cmyk(0% 0% 0% 0%)"],
+  ["WHiTE", "hsl(0 0% 100%)", "#fff", "rgb(255 255 255)", "hsl(0 0% 100%)", "hwb(0 100% 0%)", "lab(100% 0 0)", "lch(100% 0 297)", "device-cmyk(0% 0% 0% 0%)"],
+  ["yellow", "hsl(60 100% 50%)", "#ff0", "rgb(255 255 0)", "hsl(60 100% 50%)", "hwb(60 0% 0%)", "lab(97% -22 94)", "lch(97% 97 103)", "device-cmyk(0% 0% 100% 0%)"],
+  ["rgb(12, 134, 29)", "hsl(128 84% 29%)", "#0c861d", "rgb(12 134 29)", "hsl(128 84% 29%)", "hwb(128 5% 47%)", "lab(49% -51 45)", "lch(49% 68 139)", "device-cmyk(91% 0% 78% 47%)"],
+  ["hsl(230, 50%, 40%)", "hsl(230 50% 40%)", "#349", "rgb(51 68 153)", "hsl(230 50% 40%)", "hwb(230 20% 40%)", "lab(32% 22 -49)", "lch(32% 53 294)", "device-cmyk(67% 56% 0% 40%)"],
+  ["#000080", "hsl(240 100% 25%)", "#000080", "rgb(0 0 128)", "hsl(240 100% 25%)", "hwb(240 0% 50%)", "lab(13% 48 -65)", "lch(13% 80 306)", "device-cmyk(100% 100% 0% 50%)"],
+  ["rgba(199, 190, 179, 0.8)", "hsl(33 15% 74%/0.8)", "#c7beb3cc", "rgb(199 190 179/0.8)", "hsl(33 15% 74%/0.8)", "hwb(33 70% 22%/0.8)", "lab(77% 1 7/0.8)", "lch(77% 7 79/0.8)", "device-cmyk(0% 5% 10% 22%/0.8)"],
+  ["rgba(0,0,0,0)", "hsl(0 0% 0%/0)", "#0000", "rgb(0 0 0/0)", "hsl(0 0% 0%/0)", "hwb(0 0% 100%/0)", "lab(0% 0 0/0)", "lch(0% 0 0/0)", "device-cmyk(0% 0% 0% 100%/0)"],
+  ["hsla(0,0%,0%,0)", "hsl(0 0% 0%/0)", "#0000", "rgb(0 0 0/0)", "hsl(0 0% 0%/0)", "hwb(0 0% 100%/0)", "lab(0% 0 0/0)", "lch(0% 0 0/0)", "device-cmyk(0% 0% 0% 100%/0)"],
+  ["hsla(0,0%,0%,0)", "hsl(0 0% 0%/0)", "#0000", "rgb(0 0 0/0)", "hsl(0 0% 0%/0)", "hwb(0 0% 100%/0)", "lab(0% 0 0/0)", "lch(0% 0 0/0)", "device-cmyk(0% 0% 0% 100%/0)"],
+  ["hsla(0,0%,0%,0)", "hsl(0 0% 0%/0)", "#0000", "rgb(0 0 0/0)", "hsl(0 0% 0%/0)", "hwb(0 0% 100%/0)", "lab(0% 0 0/0)", "lch(0% 0 0/0)", "device-cmyk(0% 0% 0% 100%/0)"],
+  ["hsla(200,0%,0%,0)", "hsl(0 0% 0%/0)", "#0000", "rgb(0 0 0/0)", "hsl(0 0% 0%/0)", "hwb(0 0% 100%/0)", "lab(0% 0 0/0)", "lch(0% 0 0/0)", "device-cmyk(0% 0% 0% 100%/0)"],
+  ["transparent", "hsl(0 0% 0%/0)", "#0000", "rgb(0 0 0/0)", "hsl(0 0% 0%/0)", "hwb(0 0% 100%/0)", "lab(0% 0 0/0)", "lch(0% 0 0/0)", "device-cmyk(0% 0% 0% 100%/0)"],
+  ["#696969", "hsl(0 0% 41%)", "#696969", "rgb(105 105 105)", "hsl(0 0% 41%)", "hwb(0 41% 59%)", "lab(44% 0 0)", "lch(44% 0 297)", "device-cmyk(0% 0% 0% 59%)"],
+  ["rgb(400,400,400)", "hsl(0 0% 100%)", "#fff", "rgb(255 255 255)", "hsl(0 0% 100%)", "hwb(0 100% 0%)", "lab(100% 0 0)", "lch(100% 0 297)", "device-cmyk(0% 0% 0% 0%)"],
+  ["hsl(400, 400%, 50%)", "hsl(40 100% 50%)", "#fa0", "rgb(255 170 0)", "hsl(40 100% 50%)", "hwb(40 0% 0%)", "lab(76% 21 80)", "lch(76% 83 75)", "device-cmyk(0% 33% 100% 0%)"],
+  ["hsla(0, 0%, 100%, 0.5)", "hsl(0 0% 100%/0.5)", "#ffffff80", "rgb(255 255 255/0.5)", "hsl(0 0% 100%/0.5)", "hwb(0 100% 0%/0.5)", "lab(100% 0 0/0.5)", "lch(100% 0 297/0.5)", "device-cmyk(0% 0% 0% 0%/0.5)"],
+  ["rgba(-100,0,-100,.5)", "hsl(0 0% 0%/0.5)", "#00000080", "rgb(0 0 0/0.5)", "hsl(0 0% 0%/0.5)", "hwb(0 0% 100%/0.5)", "lab(0% 0 0/0.5)", "lch(0% 0 0/0.5)", "device-cmyk(0% 0% 0% 100%/0.5)"],
+  ["hsla(-400,50%,10%,.5)", "hsl(320 50% 10%/0.5)", "#260d1e80", "rgb(38 13 30/0.5)", "hsl(320 50% 10%/0.5)", "hwb(320 5% 85%/0.5)", "lab(7% 16 -6/0.5)", "lch(7% 17 340/0.5)", "device-cmyk(0% 67% 22% 85%/0.5)"],
+  ["rgb(100%,100%,100%)", "hsl(0 0% 100%)", "#fff", "rgb(255 255 255)", "hsl(0 0% 100%)", "hwb(0 100% 0%)", "lab(100% 0 0)", "lch(100% 0 297)", "device-cmyk(0% 0% 0% 0%)"],
+  ["rgba(50%,50%,50%,0.5)", "hsl(0 0% 50%/0.5)", "#80808080", "rgb(128 128 128/0.5)", "hsl(0 0% 50%/0.5)", "hwb(0 50% 50%/0.5)", "lab(54% 0 0/0.5)", "lch(54% 0 297/0.5)", "device-cmyk(0% 0% 0% 50%/0.5)"],
+  ["rgba(100%,100%,100%,0.5)", "hsl(0 0% 100%/0.5)", "#ffffff80", "rgb(255 255 255/0.5)", "hsl(0 0% 100%/0.5)", "hwb(0 100% 0%/0.5)", "lab(100% 0 0/0.5)", "lch(100% 0 297/0.5)", "device-cmyk(0% 0% 0% 0%/0.5)"],
+  ["rgba(100%,64.7%,0%,.5)", "hsl(39 100% 50%/0.5)", "#ffa50080", "rgb(255 165 0/0.5)", "hsl(39 100% 50%/0.5)", "hwb(39 0% 0%/0.5)", "lab(75% 24 79/0.5)", "lch(75% 83 73/0.5)", "device-cmyk(0% 35% 100% 0%/0.5)"],
+  ["rgb(50%,23,54)", "rgb(50%,23,54)", "rgb(50%,23,54)", "rgb(50%,23,54)", "rgb(50%,23,54)", "rgb(50%,23,54)", "rgb(50%,23,54)", "rgb(50%,23,54)", "rgb(50%,23,54)"],
+  ["Unrecognised", "unrecognised", "unrecognised", "unrecognised", "unrecognised", "unrecognised", "unrecognised", "unrecognised", "unrecognised"],
+  ["inherit", "inherit", "inherit", "inherit", "inherit", "inherit", "inherit", "inherit", "inherit"],
+  ["currentcolor", "currentcolor", "currentcolor", "currentcolor", "currentcolor", "currentcolor", "currentcolor", "currentcolor", "currentcolor"],
+]
 
-test('should convert shorthand hex to keyword', () => {
-  expect(min('#f00')).toBe('red')
-})
-
-test('should convert longhand hex to keyword', () => {
-  expect(min('#ff0000')).toBe('red')
-})
-
-test('should convert rgb to keyword', () => {
-  expect(min('rgb(255,0,0)')).toBe('red')
-})
-
-test('should convert fully opaque rgb to keyword', () => {
-  expect(min('rgba(255, 0, 0, 1)')).toBe('red')
-})
-
-test('should convert hsl to keyword', () => {
-  expect(min('hsl(0, 100%, 50%)')).toBe('red')
-})
-
-test('should convert fully oqaque hsl to keyword', () => {
-  expect(min('hsla(0, 100%, 50%, 1)')).toBe('red')
-})
-
-test('should convert translucent hsla to rgba', () => {
-  expect(min('hsla(0, 100%, 50%, .5)')).toBe('rgba(255,0,0,.5)')
-})
-
-test('should convert longhand hex to shorthand, case insensitive', () => {
-  expect(min('#FFFFFF')).toBe('#fff')
-})
-
-test('should convert keyword to hex, case insensitive', () => {
-  expect(min('WHiTE')).toBe('#fff')
-})
-
-test('should convert keyword to hex', () => {
-  expect(min('yellow')).toBe('#ff0')
-})
-
-test('should convert rgb to hex', () => {
-  expect(min('rgb(12, 134, 29)')).toBe('#0c861d')
-})
-
-test('should convert hsl to hex', () => {
-  expect(min('hsl(230, 50%, 40%)')).toBe('#349')
-})
-
-test('should convert another longhand hex to keyword', () => {
-  expect(min('#000080')).toBe('navy')
-})
-
-test('should convert rgba to hsla when shorter', () => {
-  expect(min('rgba(199, 190, 179, 0.8)')).toBe('hsla(33,15%,74%,.8)')
-})
-
-test('should convert this specific rgba value to "transparent"', () => {
-  expect(min('rgba(0,0,0,0)')).toBe('transparent')
-})
-
-test('should not convert this specific rgba value to "transparent" (legacy mode)', () => {
-  expect(min('hsla(0,0%,0%,0)', { legacy: true })).toBe('transparent')
-})
-
-test('should convert this specific hsla value to "transparent"', () => {
-  expect(min('hsla(0,0%,0%,0)')).toBe('transparent')
-})
-
-test('should convert this specific hsla value to "transparent"', () => {
-  expect(min('hsla(0,0%,0%,0)')).toBe('transparent')
-})
-
-test('should convert hsla values with 0 saturation & 0 lightness to "transparent"', () => {
-  expect(min('hsla(200,0%,0%,0)')).toBe('transparent')
-})
-
-test('should leave transparent as it is', () => {
-  expect(min('transparent')).toBe('transparent')
-})
-
-test('should prefer to output hex rather than keywords when they are the same length', () => {
-  expect(min('#696969')).toBe('#696969')
-})
-
-test('should cap values at their maximum', () => {
-  expect(min('rgb(400,400,400)')).toBe('#fff')
-})
-
-test('should cap values at their maximum (2)', () => {
-  expect(min('hsl(400, 400%, 50%)')).toBe('red')
-})
-
-test('should remove leading zeros', () => {
-  expect(min('hsla(0, 0%, 100%, 0.5)')).toBe('hsla(0,0%,100%,.5)')
-})
-
-test('should convert signed numbers', () => {
-  expect(min('rgba(-100,0,-100,.5)')).toBe('rgba(0,0,0,.5)')
-})
-
-test('should convert signed numbers (2)', () => {
-  expect(min('hsla(-400,50%,10%,.5)')).toBe('rgba(38,13,13,.5)')
-})
-
-test('should convert percentage based rgb values', () => {
-  expect(min('rgb(100%,100%,100%)')).toBe('#fff')
-})
-
-test('should convert percentage based rgba values (2)', () => {
-  expect(min('rgba(50%,50%,50%,0.5)')).toBe('hsla(0,0%,50%,.5)')
-})
-
-test('should convert percentage based rgba values (3)', () => {
-  expect(min('rgb(100%,100%,100%)')).toBe('#fff')
-})
-
-test('should convert percentage based rgba values (4)', () => {
-  expect(min('rgba(100%,100%,100%,0.5)')).toBe('hsla(0,0%,100%,.5)')
-})
-
-test('should convert percentage based rgba values (5)', () => {
-  expect(min('rgba(100%,64.7%,0%,.5)')).toBe('rgba(255,165,0,.5)')
-})
-
-test('should pass through on invalid rgb functions', () => {
-  expect(min('rgb(50%,23,54)')).toBe('rgb(50%,23,54)')
-})
-
-test('should pass through if not recognised', () => {
-  expect(min('Unrecognised')).toBe('Unrecognised')
-  expect(min('inherit')).toBe('inherit')
+describe('colornorm does as expected', () => {
+  test('default', () => {
+    for (const [input, hslDefault] of inputs) {
+      expect(colornorm(input)).toBe(hslDefault)
+    }
+  })
+  test('hex', () => {
+    for (const [input, , hex] of inputs) {
+      expect(colornorm(input, 'hex')).toBe(hex)
+    }
+  })
+  test('rgb', () => {
+    for (const [input, , , rgb] of inputs) {
+      expect(colornorm(input, 'rgb')).toBe(rgb)
+    }
+  })
+  test('hsl', () => {
+    for (const [input, , , , hsl] of inputs) {
+      expect(colornorm(input, 'hsl')).toBe(hsl)
+    }
+  })
+  test('hwb', () => {
+    for (const [input, , , , , hwb] of inputs) {
+      expect(colornorm(input, 'hwb')).toBe(hwb)
+    }
+  })
+  test('lab', () => {
+    for (const [input, , , , , , lab] of inputs) {
+      expect(colornorm(input, 'lab')).toBe(lab)
+    }
+  })
+  test('lch', () => {
+    for (const [input, , , , , , , lch] of inputs) {
+      expect(colornorm(input, 'lch')).toBe(lch)
+    }
+  })
+  test('cmyk', () => {
+    for (const [input, , , , , , , , cmyk] of inputs) {
+      expect(colornorm(input, 'cmyk')).toBe(cmyk)
+    }
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,106 @@
-import colorString from 'color-string'
-import { hslToRgb } from './hslToRgb'
-import { hwlToRgb } from './hwlToRgb'
-
-// const shorterOf = (a: string, b: string): string => (a && a.length < b.length ? a : b);
+import { parseColor, RGB } from 'css-color-l4'
+import convert from 'color-convert'
 
 export default (
-  cssColor: string,
-  opts: Partial<{ legacy: boolean }> = {}
+  cssColorString: string,
+  outputType?: 'hsl' | 'hwb' | 'lab' | 'lch' | 'cmyk' | 'rgb' | 'hex'
 ): string => {
-  const colorDetails = colorString.get(cssColor)
-  if (colorDetails === null) return cssColor
+  const output = outputType ?? 'hsl'
+  const inputString = cssColorString.toLowerCase().trim()
 
-  if (opts.legacy && colorDetails.value[3] === 0) return 'transparent'
+  const parsedColor = parseColor(inputString)
+  if (parsedColor === null) return inputString
 
-  const { model, value } = colorDetails
-
-  let rgb: [number, number, number, number]
-  if (model == 'rgb') {
-    rgb = value
-  } else {
-    if (model === 'hsl') {
-      // convert to rgb
-      rgb = hslToRgb(value)
-    } else if (model === 'hwb') {
-      // convert to rgb
-      rgb = hwlToRgb(value)
-    } else {
-      throw new Error(
-        `colornorm does not support this unknown color format (${cssColor}) - leave an issue`
-      )
-    }
+  // If alpha is zero, it is transparent, normalize to 0,0,0,0
+  const transparent: RGB = {
+    type: 'rgb',
+    r: 0,
+    g: 0,
+    b: 0,
+    alpha: 0
   }
 
-  // convert rgb to output type
+  const color = parsedColor.alpha === 0 ? transparent : parsedColor
+
+  let inputType: 'hsl' | 'hwb' | 'lab' | 'lch' | 'cmyk' | 'rgb'
+  let input: number[]
+  if (color.type === 'hsl') {
+    input = [color.h, color.s, color.l]
+    inputType = 'hsl'
+  } else if (color.type === 'hwb') {
+    input = [color.h, color.w, color.b]
+    inputType = 'hwb'
+  } else if (color.type === 'lab') {
+    input = [color.l, color.a, color.b]
+    inputType = 'lab'
+  } else if (color.type === 'lch') {
+    input = [color.l, color.c, color.h]
+    inputType = 'lch'
+  } else if (color.type === 'device-cmyk') {
+    input = [color.c, color.m, color.y, color.k]
+    inputType = 'cmyk'
+  } else {
+    input = [color.r, color.g, color.b]
+    inputType = 'rgb'
+  }
+
+  let _result: any
+  const converter = convert?.[inputType as 'rgb']?.[output as 'hsl'] ?? null
+
+  if (converter === null) {
+    _result = input
+  } else {
+    _result = converter(input as Parameters<typeof converter>)
+  }
+
+  let cssColorOut: string
+  if (output === 'hex') {
+    const result: string = _result
+
+    const alphaHex = Math.round(color.alpha * 255)
+      .toString(16)
+      .padStart(2, '0')
+    const hex = result.toLowerCase()
+    cssColorOut = '#'
+    if (alphaHex === 'ff') {
+      // 3 or 6
+      if (hex[0] === hex[1] && hex[2] === hex[3] && hex[4] === hex[5]) {
+        cssColorOut += hex[0] + hex[2] + hex[4]
+      } else {
+        cssColorOut += hex
+      }
+    } else {
+      // 4 or 8
+      if (
+        hex[0] === hex[1] &&
+        hex[2] === hex[3] &&
+        hex[4] === hex[5] &&
+        alphaHex[0] === alphaHex[1]
+      ) {
+        cssColorOut += hex[0] + hex[2] + hex[4] + alphaHex[0]
+      } else {
+        cssColorOut += hex + alphaHex
+      }
+    }
+  } else {
+    const result: number[] = _result
+    if (output === 'hsl') {
+      cssColorOut = `hsl(${result[0]} ${result[1]}% ${result[2]}%`
+    } else if (output === 'hwb') {
+      cssColorOut = `hwb(${result[0]} ${result[1]}% ${result[2]}%`
+    } else if (output === 'lab') {
+      cssColorOut = `lab(${result[0]}% ${result[1]} ${result[2]}`
+    } else if (output === 'lch') {
+      cssColorOut = `lch(${result[0]}% ${result[1]} ${result[2]}`
+    } else if (output === 'rgb') {
+      cssColorOut = `rgb(${result[0]} ${result[1]} ${result[2]}`
+    } else {
+      // if (output === "cmyk") {
+      cssColorOut = `device-cmyk(${result[0]}% ${result[1]}% ${result[2]}% ${result[3]}%`
+    }
+    // alpha
+    cssColorOut += color.alpha === 1 ? ')' : `/${color.alpha})`
+  }
+
+  return cssColorOut
 }
